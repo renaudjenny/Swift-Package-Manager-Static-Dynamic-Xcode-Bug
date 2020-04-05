c73e962049a2d76af57577bfbb139a7faffc9af4
Renaud Jenny <renaud.jenny@gmail.com>
2020-04-05 08:31:53 +0100

    Initial Commit

---
+++ Swift Package Manager Static Dynamic Xcode Bug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Package Manager Static Dynamic Xcode Bug
+//
+//  Created by Renaud JENNY on 05/04/2020.
+//  Copyright © 2020 Renaud JENNY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Package Manager Static Dynamic Xcode BugTests/Swift_Package_Manager_Static_Dynamic_Xcode_BugTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift_Package_Manager_Static_Dynamic_Xcode_BugTests.swift
+//  Swift Package Manager Static Dynamic Xcode BugTests
+//
+//  Created by Renaud JENNY on 05/04/2020.
+//  Copyright © 2020 Renaud JENNY. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_Package_Manager_Static_Dynamic_Xcode_Bug
+
+class Swift_Package_Manager_Static_Dynamic_Xcode_BugTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
